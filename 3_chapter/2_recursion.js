612a1fd143981766576e5f70b8508b6e8a72981b
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2017-01-21 20:19:43 +0000

    Adds even function

--- 3_chapter/2_recursion.js
+++ 3_chapter/2_recursion.js
@@ -1,5 +1,13 @@
 "use strict";
 
 function isEven(number) {
-  
+  if (number == 0) {
+    return true;
+  } else if (number == 1) {
+    return false;
+  } else if (number < 0) {
+    return isEven(number + 2);
+  } else {
+    return isEven(number - 2);
+  }
 }
